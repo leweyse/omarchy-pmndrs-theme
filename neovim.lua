0e6cec3bc3dbdacfe64224f136e4537c321bb218
Daryl Castro <darylcastro67@gmail.com>
2025-10-19 16:45:03 +0200

    initial commit

---
+++ neovim.lua
@@ -0,0 +1,22 @@
+return {
+	{
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('poimandres').setup({
+        bold_vert_split = false, -- use bold vertical separators
+        dim_nc_background = true, -- dim 'non-current' window backgrounds
+        disable_background = true, -- disable background
+        disable_float_background = true, -- disable background for floats
+        disable_italics = true, -- disable italics
+      })
+    end,
+  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "poimandres",
+		},
+	},
+}
