e447f5af84793812a56674933ea018f2b1630532
Daryl Castro <darylcastro67@gmail.com>
2026-01-10 16:21:13 +0100

    fix(neovim): adjust colors according to my personal preferences
    The default theme felt kinda monotonous after a while. I added some
    colors to make it more pleasant to look at.

--- neovim.lua
+++ neovim.lua
@@ -4,6 +4,20 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      local p = require('poimandres.palette')
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "*",
+        callback = function()
+          vim.api.nvim_set_hl(0, 'Quote', { link = 'Comment' })
+
+          vim.fn.matchadd('Quote', '"')
+          vim.fn.matchadd('Quote', "'")
+          vim.fn.matchadd('Quote', '`')
+          vim.fn.matchadd('Quote', 'from')
+        end,
+      })
+
       require('poimandres').setup({
         bold_vert_split = false, -- use bold vertical separators
         dim_nc_background = true, -- dim 'non-current' window backgrounds
@@ -16,6 +30,16 @@ return {
           LspReferenceText = { link = 'Visual' },
           LspReferenceRead = { link = 'Visual' },
           LspReferenceWrite = { link = 'Visual' },
+          NormalFloat = { bg = p.background1, fg = p.text },
+          FloatBorder = { bg = p.background1, fg = p.text },
+
+          ["@constructor"] = { fg = "#D66ED2" },
+          ["@keyword.coroutine"] = { fg = "#5de4c7" },
+          ["@keyword.import"] = { link = "@keyword.coroutine" },
+          ["@punctuation.bracket"] = { fg = p.yellow },
+          ["@punctuation.special"] = { link = "@constructor" },
+          ["@lsp.type.function"] = { link = "Type" },
+          ["@lsp.type.property"] = { fg = p.white },
         },
       })
     end,
